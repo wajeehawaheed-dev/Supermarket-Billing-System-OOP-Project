b853942be49aafd50e72b82bf669fe5978df748e
Muhammed Hadi Maqbool Ahmed <hadi.maqbool.ahmed8@gmail.com>
2026-04-24 22:55:32 +0500

    ..

--- SuperMarket/SuperMarket/constants.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-constexpr double      TAX_RATE = 0.17;
-constexpr int         DISCOUNT_THRESHOLD_1 = 5;
-constexpr int         DISCOUNT_THRESHOLD_2 = 10;
-constexpr double      DISCOUNT_RATE_1 = 0.10;
-constexpr double      DISCOUNT_RATE_2 = 0.20;
-constexpr const char* STORE_NAME = "FAST Supermarket";
