533734b0282308eafc4c24e1df73d65665d2b99f
RabiyaRana <rabiyarana767@gmail.com>
2026-04-25 17:12:02 +0500

    Add CardPayment class

---
+++ CardPayment.h
@@ -0,0 +1,25 @@
+#ifndef CARDPAYMENT_H
+#define CARDPAYMENT_H
+#include "Payment.h"
+#include <string>
+using namespace std;
+
+class Cardpayment :public Payment {
+	// hum ne private kia becoz kisi child class nai is ko access nahi kerna tha  as cardpayment has no child class
+private:
+	string card_no;// 16 or 15 digits 
+	string card_holder;//name 
+	string expiry;//month & year 
+	string cvv;// 3 or 4 digit 
+public:
+	Cardpayment(int billNo, double totalBill);
+	//virtual functions override 
+	bool processpayment() override;
+	void displaydetails() override;
+	// Helper Validation functions
+	bool validateCard();
+	bool validatecard_no();
+	bool validateexpiry();
+	bool validatecvv();
+};
+#endif
