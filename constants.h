32d3b3c5d1b5e49efe6eddc99f91c395a9bb8db2
Hadi <hadi.maqbool.ahmed8@gmail.com>
2026-04-25 23:34:21 +0500

    .

--- constants.h
+++ constants.h
@@ -1,8 +1,8 @@
 #pragma once
-
-constexpr double      TAX_RATE = 0.17;
-constexpr int         DISCOUNT_THRESHOLD_1 = 5;
-constexpr int         DISCOUNT_THRESHOLD_2 = 10;
-constexpr double      DISCOUNT_RATE_1 = 0.10;
-constexpr double      DISCOUNT_RATE_2 = 0.20;
-constexpr const char* STORE_NAME = "FAST Supermarket";
+extern double TAX_RATE;
+extern int DISCOUNT_THRESHOLD_1;
+extern int DISCOUNT_THRESHOLD_2;
+extern double DISCOUNT_RATE_1;
+extern double DISCOUNT_RATE_2;
+extern const char* STORE_NAME;
+;
