9068d9da54fc2ea010429259c06ec41d1c63f7f3
Rabiya Rana <l253006@lhr.nu.edu.pk>
2026-04-25 14:44:35 +0500

    Added CashPayment cpp and Header File with change calculation

---
+++ CashPayment.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include"CashPayment.h"
+using namespace std;
+	CashPayment::CashPayment(int billNO, double Amount) : Payment(billNO, Amount)
+	{
+		cashgiven = 0.0;
+		change = 0.0;
+	}
+	bool CashPayment:: processpayment()  {
+		cout << "Enter cash amount:" << endl;
+		cin >> cashgiven;
+		if (cashgiven < total_bill)
+		{
+			cout << "Insufficient Amount! Bill is" << total_bill << endl;
+			status = "Decline";
+			return false;
+		}
+		else
+		{
+			calculatechange();
+			status = "Approved";
+			paid_amount = cashgiven;
+			return true;
+		}
+
+	}
+	void  CashPayment::calculatechange() {
+		if (cashgiven == total_bill)
+			change = 0;
+		else
+		{
+			change = cashgiven - total_bill;
+			cout << "Change to return is:" << change << endl;
+
+		}
+	}
+	void  CashPayment::displaydetails() {
+		cout << "Cash Payemnt is" << endl;
+		cout << "Bill No is : " << billNo << endl;
+		cout << "Total Bill is : " << total_bill << endl;
+		cout << "Cash Given is : " << cashgiven << endl;
+		cout << "Change is: " << change << endl;
+		cout << "Status: " << status << endl;
+	}
