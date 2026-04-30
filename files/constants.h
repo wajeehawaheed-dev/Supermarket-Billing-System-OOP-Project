7f5b0b7dfa5c2640302af61b7bd5e55cbcdea4b1
Hadi <hadi.maqbool.ahmed8@gmail.com>
2026-05-01 01:40:15 +0500

    Restructure: all source files moved into SUper folder; cleanup stray files

--- files/constants.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-extern double TAX_RATE;
-extern int DISCOUNT_THRESHOLD_1;
-extern int DISCOUNT_THRESHOLD_2;
-extern double DISCOUNT_RATE_1;
-extern double DISCOUNT_RATE_2;
-extern const char* STORE_NAME;
-
